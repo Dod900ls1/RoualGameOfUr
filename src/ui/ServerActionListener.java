be43715021c7ff5501f989ebc8b09d4b6466587e
Yehor Boiar <yb33@st-andrews.ac.uk>
2024-03-19 20:06:14 +0000

    cd /cs/home/yb33/Documents/CS1006/Coursework/Project3/p3repo ; /usr/bin/env /usr/local/amazon-corretto-17/bin/java -XX:+ShowCodeDetailsInExceptionMessages -cp /home/yb33/.config/Code/User/workspaceStorage/bb92ba9bfc7bc4b225f7fc80fcf94719/redhat.java/jdt_ws/p3repo_af6d0ca4/bin ui.StartMenu

--- src/ui/ServerActionListener.java
+++ src/ui/ServerActionListener.java
@@ -15,53 +15,69 @@ public class ServerActionListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // Show a dialog to get the socket ID from the user
         JTextField socketIdField = new JTextField();
         Object[] message = { "Enter Socket ID:", socketIdField };
         int option = JOptionPane.showConfirmDialog(null, message, "Socket ID", JOptionPane.OK_CANCEL_OPTION);
 
         if (option == JOptionPane.OK_OPTION) {
-            try {
-                // Parse the socket ID entered by the user
-                int socketId = Integer.parseInt(socketIdField.getText());
-
-                // Start the server with the specified socket ID
-                ServerSocket ss = new ServerSocket(socketId);
-                System.out.println("Server started on port " + socketId + ". Waiting for client to connect...");
-                Socket s = ss.accept();
-                System.out.println("Client connected.");
-
-                DataInputStream din = new DataInputStream(s.getInputStream());
-                DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-
-                // Show a dialog to display the input/output streams
-                JOptionPane.showMessageDialog(null, "Server started on port " + socketId
-                        + ".\nClient connected.\nYou can now communicate with the client.");
-
-                // Continuously read from the client and display messages
-                String str = "", str2 = "";
-                while (!str.equals("stop")) {
-                    str = din.readUTF().toString();
-                    System.out.println("Client says: " + str);
-                    str2 = JOptionPane.showInputDialog(null, "Enter response to client:");
-                    dout.writeUTF(str2);
-                    dout.flush();
-                }
-
-                // Close resources
-                din.close();
-                dout.close();
-                s.close();
-                ss.close();
-            } catch (NumberFormatException ex) {
-                //TODO Properly handle invalid socket ID input
-                JOptionPane.showMessageDialog(null, "Invalid socket ID entered. Please enter a valid integer.");
-            } catch (IOException ex) {
-                // Handle IO exceptions
-                JOptionPane.showMessageDialog(null, "An error occurred: " + ex.getMessage());
-                // TODO We can provide instructions or suggestions to resolve the issue.
-                JOptionPane.showMessageDialog(null, "Please make sure the specified port is available and try again.");
+            int socketId = parseSocketId(socketIdField.getText());
+            if (socketId != -1) {
+                startServer(socketId);
+                showServerStartedMessage(socketId);
+            } else {
+                showInvalidSocketIdError();
             }
         }
+    }
+
+    private int parseSocketId(String socketIdText) {
+        try {
+            return Integer.parseInt(socketIdText);
+        } catch (NumberFormatException ex) {
+            return -1;
+        }
+    }
+
+    private void startServer(int socketId) {
+        try {
+            ServerSocket ss = new ServerSocket(socketId);
+            System.out.println("Server started on port " + socketId + ". Waiting for client to connect...");
+            Socket s = ss.accept();
+            System.out.println("Client connected.");
+
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
+            String str = "", str2 = "";
+            while (!str.equals("stop")) {
+                str = din.readUTF().toString();
+                System.out.println("Client says: " + str);
+                str2 = JOptionPane.showInputDialog(null, "Enter response to client:");
+                dout.writeUTF(str2);
+                dout.flush();
+            }
+
+            din.close();
+            dout.close();
+            s.close();
+            ss.close();
+        } catch (IOException ex) {
+            showServerError(ex.getMessage());
+        }
+    }
+
+    private void showServerStartedMessage(int socketId) {
+        JOptionPane.showMessageDialog(null,
+                "Server started on port " + socketId + ".\nClient connected.\nYou can now communicate with the client.");
+    }
+
+    private void showInvalidSocketIdError() {
+        JOptionPane.showMessageDialog(null, "Invalid socket ID entered. Please enter a valid integer.");
+    }
+
+    private void showServerError(String errorMessage) {
+        JOptionPane.showMessageDialog(null, "An error occurred: " + errorMessage);
+        JOptionPane.showMessageDialog(null,
+                "Please make sure the specified port is available and try again.");
     }
 }
