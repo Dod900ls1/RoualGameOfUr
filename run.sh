02d9c66f9d9b8d9454219d3bd059488380a8b096
Yehor Boiar <yb33@st-andrews.ac.uk>
2024-03-26 16:51:28 +0000

    Added run.sh, build.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+./build.sh
+case "$(uname -sr)" in
+    CYGWIN*|MINGW*|MINGW32*|MSYS*)
+        SEPARATOR=";"
+        ;;
+    *)
+    SEPARATOR=":"
+    ;;
+esac
+java -cp "lib/*${SEPARATOR}dist" main.UrMain
