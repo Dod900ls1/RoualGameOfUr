2491b69127927ce817fe6cea55d8558da827a24d
Clara Cecilia Elm Nettesheim <ccen1@st-andrews.ac.uk>
2024-03-29 15:59:18 +0000

    adding stars

--- src/ui/GameInterface.java
+++ src/ui/GameInterface.java
@@ -42,34 +42,35 @@ public class GameInterface extends JFrame{
             }
         };
 
+        ActionListener starListener = new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                JButton button = (JButton)e.getSource();
+                button.setBackground(Color.PINK);
+            }
+        };
+
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 3; j++) {
-                boardSpaces[i][j] = new JButton(" ");
-                boardSpaces[i][j].addActionListener(buttonListener);
+                if((i == 2 || i == 3) && (j == 0 || j == 2)){
+                    System.out.println(i + " : " + j);
+                    gamePanel.add(new JLabel("out box"));
+                }else if((i==4 && j==1) || (( i == 1 || i == 7) && ( j == 0 || j == 2))){
+                    boardSpaces[i][j] = new JButton("✯");
+                    boardSpaces[i][j].addActionListener(starListener);
 
-                boardSpaces[i][j].setPreferredSize(new Dimension(150,50));
-                gamePanel.add(boardSpaces[i][j]);
+                    boardSpaces[i][j].setPreferredSize(new Dimension(150,50));
+                    gamePanel.add(boardSpaces[i][j]);
+                }else{
+                    boardSpaces[i][j] = new JButton(" ");
+                    boardSpaces[i][j].addActionListener(buttonListener);
+    
+                    boardSpaces[i][j].setPreferredSize(new Dimension(150,50));
+                    gamePanel.add(boardSpaces[i][j]);
+                }
+
             }   
         }
 
-        boardSpaces[2][0].setVisible(false);
-        boardSpaces[3][0].setVisible(false);
-        boardSpaces[2][2].setVisible(false);
-        boardSpaces[3][2].setVisible(false);
-
-        // TableCellRenderer tableRenderer;
-        // JScrollPane scrollPane;
-        // JTable table = new JTable(new JTableButtonModel(boardSpaces));
-        // tableRenderer = table.getDefaultRenderer(JButton.class);
-        // table.setDefaultRenderer(JButton.class, new JTableButtonRenderer(tableRenderer));
-        // scrollPane = new JScrollPane(table);
-        // add(scrollPane, BorderLayout.CENTER);
-        /*new JTable(boardSpaces, new String[]{"Roll","Turn","Escape"});
-        table.setDefaultRenderer(Object.class, new ButtonRenderer());
-        */
-        // table.setRowHeight(50);
-        // table.setEnabled(true);
-        // add(table,CENTER_ALIGNMENT);
         gamePanel.setLayout(new GridLayout(8,3));
         add(gamePanel);
 
@@ -80,85 +81,3 @@ public class GameInterface extends JFrame{
         new GameInterface();
     }
 }
-
-class JTableButtonRenderer implements TableCellRenderer {
-
-    private TableCellRenderer defaultRenderer;
-
-    public JTableButtonRenderer(TableCellRenderer renderer) {
-       defaultRenderer = renderer;
-
-    }
-
-    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-
-        if(value instanceof Component){
-
-            return (Component)value;
-            
-        }
-
-        return defaultRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-
-    }
-}
-
-
-class JTableButtonModel extends AbstractTableModel {
-
-    ActionListener buttonListener = new ActionListener(){
-
-        public void actionPerformed(ActionEvent e){
-            JButton button = (JButton)e.getSource();
-            button.setBackground(Color.BLACK);
-        }
-
-    };
-
-    ActionListener rollListener = new ActionListener(){
-        public void actionPerformed(ActionEvent e){
-            //roll
-        }
-    };
-
-    ActionListener quitListener = new ActionListener(){
-        public void actionPerformed(ActionEvent e){
-            //go back to menu
-        }
-    };
-
-
-
-    private JButton[][] rows;
-    private JButton rollDice;
-    private JButton quitGame;
-    private Object[] columns = {rollDice,"Turn",quitGame};
-
-
-    public JTableButtonModel(JButton[][] rows){
-        rollDice = new JButton("Roll");
-        quitGame = new JButton("Quit");
-    
-        rollDice.addActionListener(rollListener);
-        quitGame.addActionListener(quitListener);
-
-        this.rows = rows;
-    }
-
-    public int getRowCount() {
-       return rows.length;
-    }
-    public int getColumnCount() {
-       return columns.length;
-    }
-    public Object getValueAt(int row, int column) {
-       return rows[row][column];
-    }
-    public boolean isCellEditable(int row, int column) {
-       return false;
-    }
-
-    public Class getColumnClass(int column) {
-       return getValueAt(0, column).getClass();
-    }
- }
