8641783f722d5c91e78a252c0e77e4c3524c16bd
Clara Cecilia Elm Nettesheim <ccen1@st-andrews.ac.uk>
2024-04-04 15:52:09 +0100

    added flowers

--- src/ui/TileInterface.java
+++ src/ui/TileInterface.java
@@ -11,6 +11,8 @@ import javax.swing.border.BevelBorder;
 
 public class TileInterface extends JButton{
     final TileController controller;
+    //1,2, or 3 : 0 if rosette or out
+    private static int[] flowerTypes = {0,1,0,1,2,1,2,3,2,3,0,3,0,1,0,0,2,0,2,3,2,3,1,3}; 
     boolean isRosette;
 
     boolean isNonWalkable;
@@ -24,23 +26,16 @@ public class TileInterface extends JButton{
 
     private void setButtonView(){
         //String text = "";
-        String location = "src/ui/flowers/";
+        String location = "src/ui/flowers/flower" + flowerTypes[controller.getTileNumber()-1] + ".png";
+        String disabledLocation = "src/ui/flowers/flower" + flowerTypes[controller.getTileNumber() - 1] + "-disabled.png";
 
-        if (isRosette){
-            //text="✯";
-            location += "Rosette.png";
-        } else if (isNonWalkable) {
-
+        if (isNonWalkable) {
             //DEBUGGING VIEW
             setText( Integer.toString(controller.getTileNumber()));
             setVisible(false); //Button only there logically to not upset grid layout
-        }else{
-            location += "Tile0.png";
-            //text= Integer.toString(controller.getTileNumber());
         }
-        //setText(text);
         setIcon(new ImageIcon(new ImageIcon(location).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
-        setDisabledIcon(new ImageIcon(new ImageIcon("src/ui/flowers/disabled.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+        setDisabledIcon(new ImageIcon(new ImageIcon(disabledLocation).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
         addActionListener(controller);
         setBorder(BorderFactory.createSoftBevelBorder(BevelBorder.RAISED));
     }
@@ -51,22 +46,26 @@ public class TileInterface extends JButton{
 
     public void updateTile() {
 
-        //TODO Make prettier?
-
         Map<Player, Integer> piecesByPlayer = controller.getPiecesByPlayer();
+
         StringBuilder sb = new StringBuilder();
+
         for (Map.Entry<Player, Integer> playerPiecesEntry : piecesByPlayer.entrySet()) {
             Integer playerColour = playerPiecesEntry.getKey().getPlayerColour();
             Integer pieceCount = playerPiecesEntry.getValue();
 
             sb.append(String.format("src/ui/pieces/%d.png", playerColour));
             this.setDisabledIcon(new ImageIcon(new ImageIcon(sb.toString()).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
-        }
-        if (sb.isEmpty()){
-            this.setDisabledIcon(new ImageIcon(new ImageIcon("src/ui/flowers/disabled.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
-            sb.append("src/ui/flowers/tile1.png");
 
         }
+
+        if (sb.isEmpty()){
+
+            this.setDisabledIcon(new ImageIcon(new ImageIcon("src/ui/flowers/flower" + flowerTypes[controller.getTileNumber() - 1] + "-disabled.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+            sb.append("src/ui/flowers/flower" + flowerTypes[controller.getTileNumber()-1] + ".png");
+
+        }
+        
         this.setIcon(new ImageIcon(new ImageIcon(sb.toString()).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
         this.repaint();
     }
