aac7c26f8644c21b40204ab3e80ff979112ff675
Yehor Boiar <yb33@st-andrews.ac.uk>
2024-04-04 20:46:08 +0100

    Poor merge conflict resolved once again...

--- src/ui/TileInterface.java
+++ src/ui/TileInterface.java
@@ -9,10 +9,10 @@ import java.awt.Image;
 import javax.swing.BorderFactory;
 import javax.swing.border.BevelBorder;
 
-public class TileInterface extends JButton{
+public class TileInterface extends JButton {
     final TileController controller;
-    //1,2, or 3 : 0 if rosette or out
-    private static int[] flowerTypes = {0,1,0,1,2,1,2,3,2,3,0,3,0,1,0,0,2,0,2,3,2,3,1,3}; 
+    // 1,2, or 3 : 0 if rosette or out
+    private static int[] flowerTypes = { 0, 1, 0, 1, 2, 1, 2, 3, 2, 3, 0, 3, 0, 1, 0, 0, 2, 0, 2, 3, 2, 3, 1, 3 };
     boolean isRosette;
 
     boolean isNonWalkable;
@@ -24,18 +24,20 @@ public class TileInterface extends JButton{
         setButtonView();
     }
 
-    private void setButtonView(){
-        //String text = "";
-        String location = "src/ui/flowers/flower" + flowerTypes[controller.getTileNumber()-1] + ".png";
-        String disabledLocation = "src/ui/flowers/flower" + flowerTypes[controller.getTileNumber() - 1] + "-disabled.png";
+    private void setButtonView() {
+        // String text = "";
+        String location = "src/ui/flowers/flower" + flowerTypes[controller.getTileNumber() - 1] + ".png";
+        String disabledLocation = "src/ui/flowers/flower" + flowerTypes[controller.getTileNumber() - 1]
+                + "-disabled.png";
 
         if (isNonWalkable) {
-            //DEBUGGING VIEW
-            setText( Integer.toString(controller.getTileNumber()));
-            setVisible(false); //Button only there logically to not upset grid layout
+            // DEBUGGING VIEW
+            setText(Integer.toString(controller.getTileNumber()));
+            setVisible(false); // Button only there logically to not upset grid layout
         }
         setIcon(new ImageIcon(new ImageIcon(location).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
-        setDisabledIcon(new ImageIcon(new ImageIcon(disabledLocation).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+        setDisabledIcon(new ImageIcon(
+                new ImageIcon(disabledLocation).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
         addActionListener(controller);
         setBorder(BorderFactory.createSoftBevelBorder(BevelBorder.RAISED));
     }
@@ -55,18 +57,22 @@ public class TileInterface extends JButton{
             Integer pieceCount = playerPiecesEntry.getValue();
 
             sb.append(String.format("src/ui/pieces/%d.png", playerColour));
-            this.setDisabledIcon(new ImageIcon(new ImageIcon(sb.toString()).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+            this.setDisabledIcon(new ImageIcon(
+                    new ImageIcon(sb.toString()).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
 
         }
 
-        if (sb.isEmpty()){
-            this.setDisabledIcon(new ImageIcon(new ImageIcon("src/ui/flowers/flower" + flowerTypes[controller.getTileNumber() - 1] + "-disabled.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
-            sb.append("src/ui/flowers/flower" + flowerTypes[controller.getTileNumber()-1] + ".png");
+        if (sb.isEmpty()) {
+            this.setDisabledIcon(new ImageIcon(new ImageIcon(
+                    "src/ui/flowers/flower" + flowerTypes[controller.getTileNumber() - 1] + "-disabled.png").getImage()
+                    .getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+            sb.append("src/ui/flowers/flower" + flowerTypes[controller.getTileNumber() - 1] + ".png");
             sb.append("src/ui/flowers/Tile1.png");
 
         }
-        
-        this.setIcon(new ImageIcon(new ImageIcon(sb.toString()).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+
+        this.setIcon(
+                new ImageIcon(new ImageIcon(sb.toString()).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
         this.repaint();
     }
 }
